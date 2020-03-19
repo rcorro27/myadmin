c38859687c98427c82a2082b12111bf4d7ec9c54
Richard Alexander Corro Chirinos <rcorroch@isimtl.local>
2020-03-19 01:30:07 -0400

    ajouter a finir

--- pages/ajouter.php
+++ pages/ajouter.php
@@ -15,11 +15,11 @@
             <h1>ajouter</h1>
             <form name="formulaire" method="post" action="actions.php">
                 <input type="hidden" name="ajouter" value="1" />
-                <p><input type="text" name="firstname" placeholder="Enter Name" required /></p>
-                <p><input type="text" name="lastname" placeholder="Enter lastname" required /></p>
-                <p><input type="text" name="email" placeholder="Enter email" required /></p>
-                <p><input type="text" name="username" placeholder="Enter username" required /></p>
-                <p><input type="text" name="password" placeholder="Enter password" required /></p>
+                <p> Prenom <input type="text" name="firstname" placeholder="Enter Name" required /></p>
+                <p> Nom <input type="text" name="lastname" placeholder="Enter lastname" required /></p>
+                <p> Courriel <input type="text" name="email" placeholder="Enter email" required /></p>
+                <p> UserName <input type="text" name="username" placeholder="Enter username" required /></p>
+                <p> Password <input type="text" name="password" placeholder="Enter password" required /></p>
                 <p><input name="submit" type="submit" value="Submit" /></p>
             </form>
         </div>

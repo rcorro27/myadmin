b216ba86e8f0e10c797d71a6d8c6a4d73703633f
Richard Alexander Corro Chirinos <rcorroch@isimtl.local>
2020-03-19 18:29:16 -0400

    bordel a ranger

--- pages/ajouter.php
+++ pages/ajouter.php
@@ -29,14 +29,26 @@ echo var_dump($valueInfo);
             <div>
                 <h1>ajouter</h1>
                 <form name="formulaire" method="post" action="actions.php">
-                    <input type="hidden" name="<?php echo (!empty($valueInfo)) ? 'modifier' : 'ajouter'; ?>" value="<?php echo (!empty($valueInfo)) ? '2' : '1'; ?>" />
-                    <p> Prenom <input type="text" name="firstname" value="<?php echo (isset($valueInfo['firstName'])) ? $valueInfo['firstName'] : ''; ?>" placeholder=" Prenom" required />
+                    <input type="hidden"
+                        name="<?php echo (!empty($valueInfo)) ? 'modifier' : 'ajouter'; ?>"
+                        value="<?php echo (!empty($valueInfo)) ? '2' : '1'; ?>" />
+                    <p> Prenom <input type="text" name="firstname"
+                            value="<?php echo (isset($valueInfo['firstName'])) ? $valueInfo['firstName'] : ''; ?>"
+                            placeholder=" Prenom" required />
                     </p>
-                    <p> Nom <input type="text" name="lastname" value="<?php echo (isset($valueInfo['lastName'])) ? $valueInfo['lastName'] : ''; ?>" placeholder="Nom" required />
+                    <p> Nom <input type="text" name="lastname"
+                            value="<?php echo (isset($valueInfo['lastName'])) ? $valueInfo['lastName'] : ''; ?>"
+                            placeholder="Nom" required />
                     </p>
-                    <p> Courriel <input type="email" name="email" value="<?php echo (isset($valueInfo['email'])) ? $valueInfo['email'] : ''; ?>" placeholder="Courriel" required /></p>
-                    <p> UserName <input type="text" name="username" value="<?php echo (isset($valueInfo['userName'])) ? $valueInfo['userName'] : ''; ?>" placeholder="Username" required /></p>
-                    <p> Password <input type="text" name="password" value="<?php echo (isset($valueInfo['password'])) ? $valueInfo['password'] : ''; ?>" placeholder="Password" required /></p>
+                    <p> Courriel <input type="email" name="email"
+                            value="<?php echo (isset($valueInfo['email'])) ? $valueInfo['email'] : ''; ?>"
+                            placeholder="Courriel" required /></p>
+                    <p> UserName <input type="text" name="username"
+                            value="<?php echo (isset($valueInfo['userName'])) ? $valueInfo['userName'] : ''; ?>"
+                            placeholder="Username" required /></p>
+                    <p> Password <input type="text" name="password"
+                            value="<?php echo (isset($valueInfo['password'])) ? $valueInfo['password'] : ''; ?>"
+                            placeholder="Password" required /></p>
                     <p><input name="submit" type="submit" value="Submit" /></p>
                 </form>
             </div>
--- pages/ind.php
+++ pages/ind.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
     <title>index.php</title>
 
@@ -76,11 +77,15 @@ if (!isset($_SESSION['User'])) {
                     <td><?=$userinfo['modificationDate']; ?>
                     </td>
 
-                    <a href="modifier.php?id=<?php echo $userinfo['id']; ?>" class="fas fa-pen-square"></a>
-                    <a href="actions.php?id=<?php echo $userinfo['id']; ?>" class="fas fa-minus-square"></a>
+                    <a href="modifier.php?id=<?php echo $userinfo['id']; ?>"
+                        class="fas fa-pen-square"></a>
+                    <a href="actions.php?id=<?php echo $userinfo['id']; ?>"
+                        class="fas fa-minus-square"></a>
 
-                    <a href="ajouter.php?id=<?php echo $userinfo['email']; ?>" class="fas fa-pen-square"></a>
-                    <a href="actions.php?id=<?php echo $userinfo['id']; ?>" class="fas fa-minus-square"></a>
+                    <a href="ajouter.php?id=<?php echo $userinfo['email']; ?>"
+                        class="fas fa-pen-square"></a>
+                    <a href="actions.php?id=<?php echo $userinfo['id']; ?>"
+                        class="fas fa-minus-square"></a>
 
                     </td>
                 </tr>

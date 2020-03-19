679ad492c91016f64e333b275f94c7eea17f9d89
Richard Alexander Corro Chirinos <rcorroch@isimtl.local>
2020-03-19 02:17:22 -0400

    test modifier

--- pages/ajouter.php
+++ pages/ajouter.php
@@ -1,3 +1,7 @@
+<?php
+include_once 'actions.php';
+?>
+
 
 <!DOCTYPE html>
 <html>
@@ -10,14 +14,16 @@
 
 <body>
     <div class="form">
-        
+
         <div>
             <h1>ajouter</h1>
             <form name="formulaire" method="post" action="actions.php">
                 <input type="hidden" name="ajouter" value="1" />
-                <p> Prenom <input type="text" name="firstname" placeholder="Enter Name" required /></p>
+                <p> Prenom <input type="text" name="firstname" placeholder="Enter Name" required
+                        value="<?php (isset($valueInfo['firstName'])) ? $valueInfo['firstName'] : ' '; ?>" />
+                </p>
                 <p> Nom <input type="text" name="lastname" placeholder="Enter lastname" required /></p>
-                <p> Courriel <input type="text" name="email" placeholder="Enter email" required /></p>
+                <p> Courriel <input type="email" name="email" placeholder="Enter email" required /></p>
                 <p> UserName <input type="text" name="username" placeholder="Enter username" required /></p>
                 <p> Password <input type="text" name="password" placeholder="Enter password" required /></p>
                 <p><input name="submit" type="submit" value="Submit" /></p>

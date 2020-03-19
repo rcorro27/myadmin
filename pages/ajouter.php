59a2664e74a7bd4d14c5a3e0fb316f516892e79d
Yahia Benhaili <ybenhail@isimtl.local>
2020-03-19 19:53:11 -0400

    update

--- pages/ajouter.php
+++ pages/ajouter.php
@@ -1,46 +1,100 @@
-<<<<<<< HEAD=======<?php
+<?php
 require_once 'conection.php';
-$valueInfo = null;
-if (isset($_REQUEST['id'])) {
-    $id = $_REQUEST['id'];
-    $sqlupdate = "SELECT * from tp_user WHERE email like '$id'";
-    $result = mysqli_query($mysqli, $sqlupdate);
-    if (mysqli_connect_errno()) {
-        echo 'Erreur de connection au serveur MySQL: ('.$mysqli->connect_errno.') '.$mysqli->connect_error;
-        exit;
-    }
-    $valueInfo = mysqli_fetch_assoc($result);
+if (isset($_GET['modifier'])) {
+    $btnName = 'Modifier';
+    $id = $_GET['modifier'];
+    $rec = mysqli_query($con, "SELECT * FROM tp_user WHERE id=$id");
+    $modified = mysqli_fetch_array($rec);
+    $id = $modified['id'];
+    $firstName = $modified['firstName'];
+    $lastName = $modified['lastName'];
+    $email = $modified['email'];
+    $userName = $modified['userName'];
+} else {
+    $btnName = 'Savegarder';
+    $firstName = '';
+    $lastName = '';
+    $email = '';
+    $userName = '';
 }
-echo var_dump($valueInfo);
 ?>
 
-    <!DOCTYPE html>
-    <html>
+<!DOCTYPE html>
+<html>
 
-    <head>
-        <meta charset="utf-8">
-        <title>ajouter</title>
-        <link rel="stylesheet" href="css/style.css" />
-    </head>
+<head>
+    <meta charset="utf-8">
+    <title>ajouter</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
+    <link rel="stylesheet" href="css/style.css" />
+</head>
 
-    <body>
-        <div class="form">
+<body>
+    <div class="form">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">MySql user Form</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Ajouter <span class="sr-only">(current)</span></a>
+                    </li>
 
-            <div>
-                <h1>ajouter</h1>
-                <form name="formulaire" method="post" action="actions.php">
-                    <input type="hidden" name="<?php echo (!empty($valueInfo)) ? 'modifier' : 'ajouter'; ?>" value="<?php echo (!empty($valueInfo)) ? '2' : '1'; ?>" />
-                    <p> Prenom <input type="text" name="firstname" value="<?php echo (isset($valueInfo['firstName'])) ? $valueInfo['firstName'] : ''; ?>" placeholder=" Prenom" required />
-                    </p>
-                    <p> Nom <input type="text" name="lastname" value="<?php echo (isset($valueInfo['lastName'])) ? $valueInfo['lastName'] : ''; ?>" placeholder="Nom" required />
-                    </p>
-                    <p> Courriel <input type="email" name="email" value="<?php echo (isset($valueInfo['email'])) ? $valueInfo['email'] : ''; ?>" placeholder="Courriel" required /></p>
-                    <p> UserName <input type="text" name="username" value="<?php echo (isset($valueInfo['userName'])) ? $valueInfo['userName'] : ''; ?>" placeholder="Username" required /></p>
-                    <p> Password <input type="text" name="password" value="<?php echo (isset($valueInfo['password'])) ? $valueInfo['password'] : ''; ?>" placeholder="Password" required /></p>
-                    <p><input name="submit" type="submit" value="Submit" /></p>
-                </form>
+
+                </ul>
+                <span class="navbar-text">
+
+                    <a class="nav-link" href="logout.php?logout">Log Out</a>
+
+                </span>
             </div>
-        </div>
-    </body>
+        </nav>
+        <form method="post" action="actions.php">
+            <input type="hidden" class="form-control" id="inputEmail3" name="id" value="<?=$id; ?>">
+            <div class="form-group row">
+                <label for="inputEmail3" class="col-sm-2 col-form-label">Preom</label>
+                <div class="col-sm-4">
+                    <input type="text" class="form-control" id="inputEmail3" name="prenom" placeholder="Entrer votre prenom" value="<?=$firstName; ?>">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="inputEmail3" class="col-sm-2 col-form-label">Nom</label>
+                <div class="col-sm-4">
+                    <input type="text" class="form-control" id="inputEmail3" name="nom" placeholder="Entrer votre nom" value="<?=$lastName; ?>">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="inputEmail3" class="col-sm-2 col-form-label">Couriel</label>
+                <div class="col-sm-4">
+                    <input type="email" class="form-control" id="inputEmail3" name="couriel" placeholder="Entrer votre couriel" value="<?=$email; ?>">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="inputEmail3" class="col-sm-2 col-form-label">Username</label>
+                <div class="col-sm-4">
+                    <input type="text" class="form-control" id="inputEmail3" name="username" placeholder="Entrer votre username" value="<?=$userName; ?>">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+                <div class="col-sm-4">
+                    <input type="password" class="form-control" id="inputPassword3" name="password" placeholder="Password">
+                </div>
+            </div>
 
-    </html>
+
+            <div class="form-group row">
+                <div class="col-sm-10">
+
+                    <button type="submit" class="btn btn-primary" name="<?=$btnName; ?>"><?=$btnName; ?></button>
+                </div>
+            </div>
+        </form>
+
+    </div>
+</body>
+
+</html>
